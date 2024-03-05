b7d5bf59fff1b910c011a7e6ff9045f788842feb
technicallyweb3 <mancinoweb3@gmail.com>
2024-03-04 22:40:50 -0700

    started adding floats

--- contracts/utils/examples/useArray.sol
+++ contracts/utils/examples/useArray.sol
@@ -7,4 +7,11 @@ import "../Array.sol";
 
 contract ExampleArray {
     using Array for uint256[];
+
+    uint256[] public someUintArray;
+    bytes[] public someBytesArray;
+
+    function pushUint256Array(uint256[] memory _memoryArray, uint256 _data) external {
+        someUintArray = _memoryArray.push();
+    }
 }
